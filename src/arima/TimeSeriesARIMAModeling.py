f33c7c590f527c3b387df1656e29e52768e781c1
Ramanathan Nachiappan <nramssn@gmail.com>
2017-04-13 00:09:21 -0700

    Images and Resutls of ARIMA model for DAL and SEA, updated presentation

--- src/arima/TimeSeriesARIMAModeling.py
+++ src/arima/TimeSeriesARIMAModeling.py
@@ -4,7 +4,7 @@ from pandas import DataFrame
 from matplotlib import pyplot
 from sklearn.metrics import mean_squared_error
 from matplotlib.pylab import rcParams
-from statsmodels.tsa.arima_model import ARMA
+from statsmodels.tsa.arima_model import ARIMA
 
 rcParams['figure.figsize'] = 15, 6
 
@@ -12,13 +12,14 @@ def parser(dates):
 	return datetime.strptime(dates, '%Y-%m-%d %H')
 
 # read the data in
-feature_SEA = "/Users/ramanathan/Google Drive/Arizona State University/Spring 2017/Statistical Machine Learning/Project/code/WeatherForecastingSML/dataset/feature_SEA.csv"
+feature_SEA = "/Users/ramanathan/Google Drive/Arizona State University/Spring 2017/Statistical Machine Learning/Project/code/WeatherForecastingSML/dataset/feature_DAL.csv"
 series = read_csv(feature_SEA, header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
 
 
 ts = series['12']
+X = ts
 
-X = DataFrame(ts.values)
+
 """dataframe = concat([values.shift(1), values], axis=1)
 dataframe.columns = ['t-1', 't+1']
 print(dataframe.head(5))
@@ -38,7 +39,7 @@ train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
 predictions = list()
 for t in range(len(test)):
-    model = ARMA(history, order=(10,0,0))
+    model = ARIMA(history, order=(5,1,0))
     model_fit = model.fit(disp=0)
     output = model_fit.forecast()
     yhat = output[0]
